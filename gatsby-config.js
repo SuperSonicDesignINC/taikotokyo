5c56576b62683268f3cc24b73817588eb94d94f9
NelsonOrlando <nelson-321@hotmail.com>
2021-03-08 23:58:47 -0300

    Change admin

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
+        
         // apiURL: `http://localhost:1337`,
         apiURL: `https://taiko-admin.herokuapp.com`,
         queryLimit: -1,
