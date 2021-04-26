73bd3be5d45aee4d800ac24fbafd2fdc6f2ba9a5
NelsonOrlando <nelson-321@hotmail.com>
2021-04-26 11:03:58 -0400

    Update insta node

--- src/hooks/useInstagram.tsx
+++ src/hooks/useInstagram.tsx
@@ -8,7 +8,7 @@ export default function () {
             localFile {
               childImageSharp {
                 fluid {
-                  ...GatsbyImageSharpFluid_withWebp
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
