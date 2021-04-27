d82f921d24eb29c2161b5461b3a3ccfbe96ae5a0
NelsonOrlando <nelson-321@hotmail.com>
2021-04-27 12:09:34 -0400

    Update insta

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
   const { createTypes } = actions
   const typeDefs = [
     schema.buildObjectType({
-      name: "googleSheetDbRow",
+      name: "allInstaNode",
       fields: {
         username: "String!",
       },
